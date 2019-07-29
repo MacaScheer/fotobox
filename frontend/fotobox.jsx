076a6bfa112f119779a05afb29900784e21171c6
Michael_Scheer <macas.scheer@gmail.com>
2019-07-29 16:28:27 -0700

    Rename app

--- frontend/fotobox.jsx
+++ frontend/fotobox.jsx
@@ -3,5 +3,5 @@ import ReactDOM from 'react-dom';
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
-    ReactDOM.render(<h1>InstagramClone</h1>, root);
+    ReactDOM.render(<h1>Foto-Box</h1>, root);
 });
