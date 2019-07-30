3bbca46885a6ebdf83d96ab013aa2df4e82d78df
Michael_Scheer <macas.scheer@gmail.com>
2019-07-29 20:40:22 -0700

    changed database config from jquery3 to postgresql

--- app/controllers/api/session_controller.rb
+++ app/controllers/api/session_controller.rb
@@ -1,2 +1,25 @@
 class SessionController < ApplicationController
+     def create
+        @user = User.find_by_credentials(
+            params[:user][:username],
+            params[:user][:password]
+        )
+
+        if @user
+            sign_in(@user)
+            render "/api/users/show"
+        else
+            render json: ["invalid credentials"], status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+       if logged_in?
+            sign_out
+            output = {}
+            render json: output
+       else
+            render json: ["error"], status: 405 
+        end
+    end
 end
