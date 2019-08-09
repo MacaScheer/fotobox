4de0902e022e5816996fefc7aa9330f71a399ace
Michael_Scheer <macas.scheer@gmail.com>
2019-08-08 20:05:35 -0700

    tried to fix create_post form

--- frontend/components/posts/create_post_container.js
+++ frontend/components/posts/create_post_container.js
@@ -1,16 +1,20 @@
 import CreatePost from './create_post';
 import { connect } from 'react-redux';
-
+import { receiveResetErrors } from '../../actions/session_actions';
 
 const mapStateToProps = (state) => {
     return {
+        errors: state.errors.session,
         currentUser: state.entities.users[state.session.id],
         photo: "", title: "", location: ""
     }
 }
 
 const mapDispatchToProps = (dispatch) => {
-    return { createPost: (post) => dispatch(createPost(post)) }
+    return {
+        createPost: (post) => dispatch(createPost(post)),
+        clearErrors: () => dispatch(receiveResetErrors())
+    }
 }
 
 export default connect(
