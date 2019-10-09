c5695bde2168bd6abcfca16fc70411021e769159
Michael_Scheer <macas.scheer@gmail.com>
2019-10-09 19:03:20 -0400

    add img_url to posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_23_213208) do
+ActiveRecord::Schema.define(version: 2019_10_09_225948) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(version: 2019_08_23_213208) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "img_url"
     t.index ["location"], name: "index_posts_on_location"
     t.index ["title"], name: "index_posts_on_title"
     t.index ["user_id"], name: "index_posts_on_user_id"
