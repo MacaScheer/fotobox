af024d6873383a57e96465ff58db491487084dbd
Michael_Scheer <macas.scheer@gmail.com>
2019-10-11 09:32:38 -0400

    track down db bug

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,11 +2,18 @@ class Post < ApplicationRecord
     validates :title, :location, presence: true
 
     has_one_attached :photo
+    belongs_to :user
     
-    
-    # has_many :likes
-    # has_many :comments
-    # belongs_to :user
+    has_many :likes,
+    dependent: :destroy
+
+
+    has_many :likers,
+    through: :likes,
+    source: :user
+
+    has_many :comments,
+    dependent: :destroy
  
     # def self.find_user_posts(user_id)
     #     posts = Post.where(user_id: user_id)
