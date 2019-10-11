728315646c0823543bdcb4ed5f1c90ec065ce6e0
Michael_Scheer <macas.scheer@gmail.com>
2019-10-11 12:05:15 -0400

    fix seed file and bucket access

--- frontend/components/posts/post_index_container.js
+++ frontend/components/posts/post_index_container.js
@@ -1,25 +1,26 @@
-import PostIndex from './post_index';
-import { withRouter } from 'react-router';
+import PostIndex from "./post_index";
+import { withRouter } from "react-router";
 // import store from '..store/store';
-import { fetchPosts } from '../../actions/post_actions';
-import { connect } from 'react-redux';
-import { logout } from '../../actions/session_actions';
+import { fetchPosts } from "../../actions/post_actions";
+import { fetchUser } from "../../actions/user_actions";
+import { connect } from "react-redux";
+import { logout } from "../../actions/session_actions";
 
-const mapStateToProps = (state) => {
-    return { posts: Object.values(state.entities.posts) }
-}
+const mapStateToProps = state => {
+  return { posts: Object.values(state.entities.posts) };
+};
 
-const mapDispatchToProps = (dispatch) => {
-    return {
-        fetchPosts: () => dispatch(fetchPosts()),
-        logout: () => dispatch(logout()),
-    }
-}
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchUser: id => dispatch(fetchUser(id)),
+    fetchPosts: () => dispatch(fetchPosts()),
+    logout: () => dispatch(logout())
+  };
+};
 
-
-
-
-export default withRouter(connect(
+export default withRouter(
+  connect(
     mapStateToProps,
     mapDispatchToProps
-)(PostIndex));
+  )(PostIndex)
+);
--- frontend/components/posts/post_index_item.jsx
+++ frontend/components/posts/post_index_item.jsx
@@ -1,35 +1,41 @@
-import React from 'react';
-import { withRouter } from 'react-router';
-import { Link } from 'react-router-dom'
+import React from "react";
+import { withRouter } from "react-router";
+import { Link } from "react-router-dom";
 
 class PostIndexItem extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
+  componentDidMount() {
+    this.props.fetchUser(this.props.match.params.userId);
+  }
 
-
-    render() {
-
-        return (
-            <li className="feed-image-box">
-                <div className="feed-image-header">
-                    <div className="feed-image-header-wrap">
-                        {/* <Link to={`/users/${post.user_id}`}> */}
-                        <img className="feed-profile-pic" src="https://fotobox-seeds.s3-us-west-1.amazonaws.com/carozzi.jpg" />
-                        {/* </Link> */}
-                        <Link className="profile-link" to={`/users/${this.props.user_id}`}>dangatangg demo</Link>
-                    </div>
-                    <div>
-                        <Link to={`/posts/${this.props.id}`} >
-                            <img className="feed-image" src={this.props.photo_url} />
-                        </Link>
-                        <span>{this.props.title}</span>
-                    </div>
-                </div>
-            </li >
-
-        )
-    }
+  render() {
+    debugger;
+    return (
+      <li className="feed-image-box">
+        <div className="feed-image-header">
+          <div className="feed-image-header-wrap">
+            {/* <Link to={`/users/${post.user_id}`}> */}
+            <img
+              className="feed-profile-pic"
+              src={this.props.user.profile_picture}
+            />
+            {/* </Link> */}
+            <Link className="profile-link" to={`/users/${this.props.user_id}`}>
+              dangatangg demo
+            </Link>
+          </div>
+          <div>
+            <Link to={`/posts/${this.props.id}`}>
+              <img className="feed-image" src={this.props.photo_url} />
+            </Link>
+            <span>{this.props.title}</span>
+          </div>
+        </div>
+      </li>
+    );
+  }
 }
 
 export default withRouter(PostIndexItem);
