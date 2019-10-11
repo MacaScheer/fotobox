c98c12a0b39b2cf988a68fe36cebaa67fa242661
Michael_Scheer <macas.scheer@gmail.com>
2019-10-11 12:05:59 -0400

    remove debuggers from master

--- frontend/components/posts/post_index_item.jsx
+++ frontend/components/posts/post_index_item.jsx
@@ -11,7 +11,7 @@ class PostIndexItem extends React.Component {
   }
 
   render() {
-    debugger;
+    // debugger;
     return (
       <li className="feed-image-box">
         <div className="feed-image-header">
