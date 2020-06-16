bc3e1109ba573b5770334fed86e42ad2a2ddfe64
Michael_Scheer <macas.scheer@gmail.com>
2020-06-16 13:11:47 -0700

    remove unused code

--- frontend/components/profile/profile.jsx
+++ frontend/components/profile/profile.jsx
@@ -1,7 +1,4 @@
 import React from "react";
-import { findDOMNode } from 'react-dom';
-import NavBarContainer from "../nav/nav_bar_container";
-// import { Waypoint } from "react-waypoint";
 import ProfileIndexItem from "./profile_index_item";
 // import useInfiniteScroll from "./useInfiniteScroll";
 import ProfileList from "./profile_list";
@@ -32,44 +29,17 @@ class Profile extends React.Component {
     this.getPosts()
     this.props.fetchNumPosts(this.props.currentUser.id);
     this.props.fetchUser(this.props.currentUser.id);
-    // const scroll = document.getElementsByClassName('profile-photo-index')[0]
-    // document.addEventListener('scroll', this.scroller)
-    // scroll.addEventListener("scroll", this.scroller);
-    // this.scroller();
     document.addEventListener('scroll', this.scroller)
     this.props.closeModal();
   }
   componentWillUnmount() {
-    // const scroll = document.getElementsByClassName('profile-photo-index')[0]
-    // scroll.removeEventListener('scroll', this.scroller)
     document.removeEventListener('scroll', this.scroller)
   }
   
   scroller() {
-    // console.log("infiniteSCroller!")
-    // window.onscroll = debounce(() => {
-      let innerHeight = window.innerHeight;
-      let { scrollHeight } = document.documentElement;
-      let { offsetHeight } = document.documentElement;
-      let { scrollTop } = document.documentElement;
-      // debugger
-    console.log("SCROLL!", innerHeight + scrollTop, offsetHeight)
-      // if (
-      //   window.innerHeight + document.documentElement.scrollTop ===
-      //   document.documentElement.offsetHeight
-      //  || window.scrollY > 90
-      // ) {
-      //   this.getPosts()
-      // }
-        // },10)
-    // window.onscroll = function (ev) {
-      console.log("Scrolling")
     if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-      console.log("inside if logic")
-        // you're at the bottom of the page
       this.getPosts()
     }
-// };
     }
 
   handleNewPostForm(e) {
@@ -100,7 +70,6 @@ class Profile extends React.Component {
     });
     return (
       <div >
-        {/* <NavBarContainer /> */}
         <div className="profile-wrap">
           <div className="profile-left"></div>
           <div className="profile-container">
