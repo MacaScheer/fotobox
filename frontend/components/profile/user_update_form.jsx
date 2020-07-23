92ce221377ef27d6816e47fa1fb39e3be21ee63f
Michael_Scheer <macas.scheer@gmail.com>
2020-07-23 15:49:21 -0700

    finalized create_post component styling

--- frontend/components/profile/user_update_form.jsx
+++ frontend/components/profile/user_update_form.jsx
@@ -63,21 +63,21 @@ class UserUpdateForm extends React.Component {
     this.props.history.push(path);
   }
   render() {
+    
     let postPreview = (
       <div className="preview-div update-preview">
         <div className="update-left-top">Profile Picture</div>
         <div className="profile-pic-main">
-          <img className="post-pic-preview" src={this.state.photoUrl} />
+          <img className="post-pic-preview" src={this.state.photoFile} />
         </div>
       </div>
     );
-
+debugger
     return (
-      <div>
+      <div className="post-form-container">
         <NavBarContainer />
-        <div className="post-form-container">
-          <ul className="login-errors">{/* {this.errors()} */}</ul>
-          <div className="upload-form-div">{postPreview}</div>
+          {/* <ul className="login-errors">{this.errors()}</ul> */}
+          <div className="scanner-icon">{postPreview}</div>
 
           <form className="post-form" onSubmit={this.handleSubmit}>
             <div className="update-form-right">
@@ -88,14 +88,18 @@ class UserUpdateForm extends React.Component {
                 <div className="update-profile-pic">
                   <label className="upload-photo" htmlFor="file-selector">
                     <div className="update-profile-text">
-                      update profile picture:
+                    update profile picture:
                     </div>
-                    <input
+                      <div className="box">
+					              <input type="file" name="file-5[]" id="file-5" className="inputfile inputfile-4" onChange={this.handleFile} data-multiple-caption="{count} files selected" multiple />
+				    	          <label htmlFor="file-5"><figure><svg xmlns="http://www.w3.org/2000/svg" width="20" height="17" viewBox="0 0 20 17"><path d="M10 0l-5.2 4.9h3.3v5.1h3.8v-5.1h3.3l-5.2-4.9zm9.3 11.5l-3.2-2.1h-2l3.4 2.6h-3.5c-.1 0-.2.1-.2.1l-.8 2.3h-6l-.8-2.2c-.1-.1-.1-.2-.2-.2h-3.6l3.4-2.6h-2l-3.2 2.1c-.4.3-.7 1-.6 1.5l.6 3.1c.1.5.7.9 1.2.9h16.3c.6 0 1.1-.4 1.3-.9l.6-3.1c.1-.5-.2-1.2-.7-1.5z"/></svg></figure> <span>Choose a file&hellip;</span></label>
+				              </div>
+                    {/* <input
                       className="photo-input-field int"
                       id="file-selector"
                       type="file"
                       onChange={this.handleFile}
-                    />
+                    /> */}
                   </label>
                 </div>
                 <label className="update-email">
@@ -153,7 +157,6 @@ class UserUpdateForm extends React.Component {
             </div>
           </form>
         </div>
-      </div>
     );
   }
   // render() {
